8a6ff8736f54039516dbb002931063e2d21997e4
chengpengli <jingzhaolicheng@163.com>
2021-04-19 21:54:24 +0800

    add more fields to ignore

--- xstream/src/java/com/thoughtworks/xstream/mapper/ElementIgnoringMapper.java
+++ xstream/src/java/com/thoughtworks/xstream/mapper/ElementIgnoringMapper.java
@@ -47,16 +47,57 @@ public class ElementIgnoringMapper extends MapperWrapper {
         } else if (definedIn == Object.class && isIgnoredElement(fieldName)) {
             return false;
         }
-        // Hack to ignore field of type CodeSource
         try {
-            if (definedIn.getField(fieldName).getType().equals(java.security.CodeSource.class)) {
+            // Hack to ignore field of type CodeSource
+            System.out.println("IGNORING: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.security.CodeSource"))) {
+                System.out.println("IGNORING CodeSource: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
                 return false;
             }
-            if (definedIn.getField(fieldName).getType().equals(sun.instrument.InstrumentationImpl.class)) {
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("sun.nio.cs.UTF_8$Decoder"))) {
+                System.out.println("IGNORING UTF_8$Decoder: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
                 return false;
             }
-        } catch (NoSuchFieldException nsfe) {
-            // Ignore
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.nio.charset.CharsetEncoder"))) {
+                System.out.println("IGNORING CharsetEncoder: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.nio.charset.CharsetDecoder"))) {
+                System.out.println("IGNORING CharsetDecoder: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            } 
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("sun.nio.cs.StreamEncoder"))) {
+                System.out.println("IGNORING StreamEncoder: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.util.zip.ZipCoder"))) {
+                System.out.println("IGNORING ZipCoder: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("com.sun.crypto.provider.SunJCE"))) {
+                System.out.println("IGNORING SunJCE: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.lang.ClassLoader"))){
+                System.out.println("IGNORING ClassLoader: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.security.SecureClassLoader"))){
+                System.out.println("IGNORING SecureClassLoader: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+            if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("java.security.Provider"))) {
+                System.out.println("IGNORING java.security.Provider: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+	        if (definedIn.getDeclaredField(fieldName).getType().equals(Class.forName("javax.security.auth.Subject"))){
+                System.out.println("IGNORING auth.Subject: " + definedIn.getDeclaredField(fieldName).getType() + " - " + fieldName);
+                return false;
+            }
+        } catch (Exception exception){
+            // ignore
+            System.out.println("EXCEPTION IN IGNORING:" + fieldName + " - " + exception);
         }
         return super.shouldSerializeMember(definedIn, fieldName);
     }
